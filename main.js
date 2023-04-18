0e1d3f059be8a50512317e1bf730be90e2979153
Calli Herrmann <calliherrmann@gmail.com>
2023-04-18 19:47:40 -0400

    Add create player functionality

--- main.js
+++ main.js
@@ -19,3 +19,36 @@ function playClassic() {
 function getRandomIndex(array) {
     return Math.floor(Math.random() * array.length);
 }
+
+function createPlayer(name, token) {
+    var player = {
+        name,
+        token,
+        wins: 0,
+    }
+    return player;
+}
+
+function takeTurn() {
+
+}
+
+function createGame() {
+    //should return a game object containing:
+        // Two Player objects (player1 and player2)
+        // A way to keep track of the data for the game board
+        // A way to keep track of the selected game type
+}
+
+// A separate function to check the game’s board data for win conditions
+function winConditions() {
+
+}
+// A separate function to detect when a game is a draw (no one has won)
+function draw() {
+
+}
+// A separate function to reset the game’s board to begin a new game
+function resetGame() {
+    
+}
