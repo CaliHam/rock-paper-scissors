9669321d708a0ea557075be9361993caa2de2dbf
Calli Herrmann <calliherrmann@gmail.com>
2023-04-19 17:40:35 -0400

    Add functionality to play classic game on data model continuously

--- main.js
+++ main.js
@@ -5,13 +5,19 @@ var playClassicOptions = document.querySelector('#classic-options')
 var playerOne = document.querySelector('.human')
 var playerTwo = document.querySelector('.comp')
 var changeGameBtn = document.querySelector('.change-game')
+var userChoice = document.querySelectorAll('.choice')
 
-var currentGame = createGame(createPlayer('Human', `:)`), createPlayer('Computer', `[>:)]`))
+var currentGame = createGame(createPlayer('Human', `assets/neutral.png`), createPlayer('Computer', `assets/comp-neutral.png`))
 var human = currentGame.players[0]
 var computer = currentGame.players[1]
 
 // EVENT LISTENERS //
 classicChoice.addEventListener('click', playClassic)
+userChoice.forEach((element) => element.addEventListener('click', function(e) {
+    takeTurn(human, e)
+    takeTurn(computer)
+    pickWinner(human, computer)
+}));
 
 window.onload = function() {
     setMatch(human, computer)
@@ -23,10 +29,9 @@ function playClassic() {
     playClassicOptions.classList.remove('hidden')
 
     currentGame.type = 'classic';
-
-    takeTurn(human, userChoice)
-    takeTurn(computer)
 }
+
+
 
 function setMatch(player1, player2){
     playerOne.innerHTML = `
@@ -67,27 +72,48 @@ function createGame(player1, player2) {
     return newGame;
 }
 
-function takeTurn(player, userChoice) {
-    if (player.name === 'Human'){
-      player.choice = userChoice
-      console.log('human chose:', player.choice)
+function takeTurn(player, event) {
+    if (event){
+        var userChoice = event.target.alt
+        player.choice = userChoice
+        console.log('human chose:', player.choice)
     } else {
-      var i = getRandomIndex(currentGame.options[currentGame.type])
+        var i = getRandomIndex(currentGame.options[currentGame.type])
         player.choice = currentGame.options[currentGame.type][i]
         console.log('computer chose:', player.choice)
     }
     return player;
-  }
+}
 
 // A separate function to check the game’s board data for win conditions
-function winConditions() {
-
+function pickWinner(player1, player2) {
+    draw(player1, player2)
+        for (var i = 0; i < currentGame.winConditions.length; i++) {
+            if (player1.choice === currentGame.winConditions[i][0] && player2.choice === currentGame.winConditions[i][1]) {
+            player1.wins += 1
+            console.log('player 1 wins!')
+            resetGame();
+        } 
+        else if (player2.choice === currentGame.winConditions[i][0] && player1.choice === currentGame.winConditions[i][1]) {
+            player2.wins += 1
+            console.log('player 2 wins!')
+            resetGame();
+        }
+    }
 }
 // A separate function to detect when a game is a draw (no one has won)
-function draw() {
-
+function draw(player1, player2) {
+    if (player1.choice === player2.choice) {
+        console.log('its a draw!')
+        resetGame();
+    }
+    else {
+        return;
+    }
 }
 // A separate function to reset the game’s board to begin a new game
 function resetGame() {
-
-}
+    setMatch(human, computer)
+    // playClassicOptions.classList.remove('hidden')
+    playClassic()
+  }
