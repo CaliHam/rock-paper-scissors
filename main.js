995af9de873a1b71444f83b3f040222f4752be0f
Calli Herrmann <calliherrmann@gmail.com>
2023-04-18 21:38:03 -0400

    Add updated icons on page load

--- main.js
+++ main.js
@@ -2,11 +2,17 @@
 var classicChoice = document.querySelector('#classic')
 var homeView = document.querySelector('#home-view')
 var playClassicOptions = document.querySelector('#classic-options')
-
+var playerOne = document.querySelector('.human')
+var playerTwo = document.querySelector('.comp')
 
 // EVENT LISTENERS //
 classicChoice.addEventListener('click', playClassic)
 
+window.onload = function() {
+    var human = createPlayer('Human', `assets/neutral.png`)
+    var computer = createPlayer('Computer', `assets/comp-neutral.png`)
+    setMatch(human, computer)
+}
 
 // EVENT HANDLERS //
 function playClassic() {
@@ -14,7 +20,16 @@ function playClassic() {
     playClassicOptions.classList.remove('hidden')
 }
 
-
+function setMatch(player1, player2){
+    playerOne.innerHTML = `
+        <img src="${player1.token}" class="player-icons" alt="${player1.name} Icon">
+        <h4>${player1.name}</h4>
+        <p>Wins: ${player1.wins}</p>`
+    playerTwo.innerHTML = `
+        <img src="${player2.token}" class="player-icons" alt="${player2.name} Icon">
+        <h4>${player2.name}</h4>
+        <p>Wins: ${player2.wins}</p>`
+}
 // OTHER FUNCTIONS //
 function getRandomIndex(array) {
     return Math.floor(Math.random() * array.length);
@@ -33,7 +48,12 @@ function takeTurn() {
 
 }
 
-function createGame() {
+function createGame(player1, player2) {
+    var currentGame = {
+        players: [player1, player2],
+        type: type? classic : elemental,
+    }
+    return currentGame;
     //should return a game object containing:
         // Two Player objects (player1 and player2)
         // A way to keep track of the data for the game board
@@ -50,5 +70,5 @@ function draw() {
 }
 // A separate function to reset the game’s board to begin a new game
 function resetGame() {
-    
+
 }
